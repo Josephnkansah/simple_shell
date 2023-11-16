e0e07b4556fbd30761dde7b830030e1a05cc270c
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2023-11-16 02:48:14 +0000

    Update find_path.c

--- find_path.c
+++ find_path.c
@@ -57,7 +57,7 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 
 	if (!pathstr)
 		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	if ((ourtop_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
 		if (is_cmd(info, cmd))
 			return (cmd);
